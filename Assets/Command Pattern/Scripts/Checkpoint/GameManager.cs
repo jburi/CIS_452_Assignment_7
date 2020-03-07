7112c65301d05f1e689c04f978d1836abce87bc6
Jake Buri <jburi@mail.bradley.edu>
2020-03-06 23:58:09 -0600

    first commit

---
+++ Assets/Command Pattern/Scripts/Checkpoint/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public bool checkpointHit;
+	public bool finishHit;
+
+	private void Update()
+	{
+		if (checkpointHit == true && finishHit == true)
+		{
+			SceneManager.LoadScene("Win");
+		}
+	}
+}
